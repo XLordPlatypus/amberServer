c54eb96c039a6dacdd0737a5b48394e2c226e709
Leandro Aepli <gitlab.gecko075@passmail.net>
2025-04-28 23:53:24 +0200

    ongoing

--- src/controllers/snakeController.ts
+++ src/controllers/snakeController.ts
@@ -1,1 +1,35 @@
+import {getAllSnakes, getSnake} from "../services/snakeService";
 
+
+// TODO: Statuscodes?
+const getAll = () => {
+    try {
+        return { success: true, data: getAllSnakes() }
+    } catch (e) {
+        return { success: false, data: e }
+    }
+}
+
+const get = (id: number) => {
+    try {
+        return getSnake(id)
+    } catch (e) {
+
+    }
+}
+
+const create = () => {
+    try {
+
+    } catch (e) {
+
+    }
+}
+
+const update = () => {
+    try {
+
+    } catch (e) {
+
+    }
+}
